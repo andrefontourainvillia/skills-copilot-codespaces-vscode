b3747227bad18831302ed93f0f2c6e5d01e9f5f6
andrefontourainvillia <140420144+andrefontourainvillia@users.noreply.github.com>
2025-01-08 17:33:06 +0000

    Add skillsMembers function to members.js

---
+++ members.js
@@ -0,0 +1,12 @@
+function skillsMembers() {
+    return [
+        {
+            name: 'John Doe',
+            skills: ['JavaScript', 'React', 'Node']
+        },
+        {
+            name: 'Jane Doe',
+            skills: ['JavaScript', 'Angular', 'Node']
+        }
+    ];
+}
