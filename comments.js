2e3d120f5a3036957d872d042b21b0be9302c252
andrefontourainvillia <140420144+andrefontourainvillia@users.noreply.github.com>
2025-01-08 17:53:24 +0000

    Add comments API with CRUD operations in comments.js

---
+++ comments.js
@@ -0,0 +1,70 @@
+// Create web server
+// Run the server
+// Create a route for GET /comments
+// Create a route for POST /comments
+// Create a route for GET /comments/:id
+// Create a route for DELETE /comments/:id
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/comments', (req, res) => {
+  fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.send(JSON.parse(data));
+    }
+  });
+});
+
+app.post('/comments', (req, res) => {
+  fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      const comments = JSON.parse(data);
+      comments.push(req.body);
+
+      fs.writeFile(path.join(__dirname, 'comments.json'), JSON.stringify(comments, null, 2), (err) => {
+        if (err) {
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.send('Comment added successfully');
+        }
+      });
+    }
+  });
+});
+
+app.get('/comments/:id', (req, res) => {
+  fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      const comments = JSON.parse(data);
+      const comment = comments.find((comment) => comment.id === parseInt(req.params.id));
+
+      if (comment) {
+        res.send(comment);
+      } else {
+        res.status(404).send('Comment not found');
+      }
+    }
+  });
+});
+
+app.delete('/comments/:id', (req, res) => {
+  fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      const comments = JSON.parse(data);
+      const commentIndex = comments.findIndex((comment) => comment.id === parseInt(req.params.id));
+
+      if (commentIndex
